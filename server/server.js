b9a4ae0e24d563bcfdc4411323b8e8eb849524c7
Neil Lambert <neil.lambert@fundingcircle.com>
2019-05-11 11:24:59 -0700

    Movie API Client fetches most popular movies (#2)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const dotenv = require('dotenv'); 
+dotenv.config();
 
-app.get('/', (req, res) => res.send('Online'))
+const port = process.env.PORT;
+const API_KEY = process.env.TMDB_KEY
 
-app.listen(port, () => console.log('Backend service listening on port ${port}'))
+const express = require('express');
+const app = express(); 
+
+const axios = require('axios');
+
+const movie_db = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/movie',
+  timeout: 2000,
+  params: {
+    api_key: API_KEY
+  }
+});
+
+app.get('/', (req, res) => {
+  movie_db.get('/popular')
+    .then(function (api_response) {
+      res.send(api_response.data);
+    })
+});
+
+app.listen(port, () => console.log("Backend service listening on port", port))
