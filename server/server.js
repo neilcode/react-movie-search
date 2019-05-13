86e4839f5c9569b3d64e15156fbe85121cf32bad
Neil Lambert <neil.lambert@fundingcircle.com>
2019-05-12 19:08:51 -0700

    code cleanup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
-const dotenv = require('dotenv'); 
-dotenv.config();
-
-const cors = require('cors')
-
-const API_KEY = process.env.TMDB_KEY
-
+const dotenv  = require('dotenv'); 
+const cors    = require('cors')
 const express = require('express');
-const app = express(); 
+const app     = express(); 
+const axios   = require('axios');
 
-const axios = require('axios');
+// Setup configuration
+dotenv.config();
+const API_KEY = process.env.TMDB_KEY
+//
+// Because I chose to separate client and server
+app.use(cors({
+  origin: '*'
+  //In a production environment we would want to 
+  //whitelist specific origins
+}));
 
 const movie_db = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
@@ -18,52 +23,57 @@ const movie_db = axios.create({
   }
 });
 
-// Because I chose to separate client and server
-app.use(cors({
-  origin: '*'
-  //In a production environment we would want to 
-  //whitelist specific origins
-}));
-
 // ROUTES
 
+//
+// fetch the first page of popular movies from
+// TMDB and forward the response.
 app.get('/most_popular', (req, res) => {
   movie_db.get(
     '/movie/popular'
   )
-  .then(function (api_response) {
+  .then(api_response => {
     res.send(api_response.data.results);
   })
-  .catch(error_response => { console.log(error_response) });
+  .catch(error_response => { console.log(error_response) }); // Normally we'd want proper error logging in production!
 });
 
+//
+// fetch the first page of Now Playing movies
+// from TMDB and forward response.
 app.get('/now_playing', (req, res) => {
   movie_db.get(
     '/movie/now_playing'
   )
-  .then(function (api_response) {
+  .then(api_response => {
     res.send(api_response.data.results);
   })
-  .catch(error_response => { console.log(error_response) });
+  .catch(error_response => { console.log(error_response) }); // Normally we'd want proper error logging in production!
 });
+
+//
 // search route captures anything from the 'title' query string parameters and
 // sends to the MovieDB search endpoint. In a production system we would want to 
 // sanitize the input. Sanitization could also be implemented in the Client UI.
-app.get('/search', function (req, res) {
+// I've included the flag to omit adult movies after searching for 'Open Season'. 
+// Yikes.
+app.get('/search', (req, res) => {
   movie_db.get('/search/movie', {
     params: {
       query: req.query.title,
       include_adult: false
     }
   })
-  .then(function (api_response) {
+  .then(api_response => {
     res.send(api_response.data);
   })
-  .catch(function (error_response) {
+  .catch(error_response => {
     console.log(error_response);
   })
 })
 
+//
+// Fetch details for a specific specific film
 app.get('/movies/:movieId', (req, res) => {
   movie_db.get(
     '/movie/' + req.params.movieId
